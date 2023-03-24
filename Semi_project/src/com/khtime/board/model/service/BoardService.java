f760e4d92773eb702040009310a0d0243efa5a0b
th816 <th816@naver.com>
2023-03-24 14:06:20 +0900

    2023-03-24
    board

--- Semi_project/src/com/khtime/board/model/service/BoardService.java
+++ Semi_project/src/com/khtime/board/model/service/BoardService.java
@@ -1,5 +1,36 @@
 package com.khtime.board.model.service;
 
+import static com.khtime.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.khtime.board.model.vo.Board;
+import com.khtime.common.model.vo.PageInfo;
+import com.khtime.board.model.dao.BoardDao;
+
 public class BoardService {
+	
+	public int selectListCount() {
+	      Connection conn = getConnection();
+	      
+	      int listCount = new BoardDao().selectListCount(conn);
+	      
+	      close(conn);
+	      
+	      return listCount;
+	   }
+	
+	
+	public ArrayList<Board> selectList(PageInfo pi) {
+	      
+	      Connection conn = getConnection();
+	      
+	      ArrayList<Board> list = new BoardDao().selectList(conn, pi);
+	      
+	      close(conn);
+	      
+	      return list;
+	   }
 
 }
