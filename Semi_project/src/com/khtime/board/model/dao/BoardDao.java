0070be7d9c773fba2b5abaccb3fe81817629e2f7
th816 <th816@naver.com>
2023-03-24 15:19:10 +0900

    Update BoardDao.java

--- Semi_project/src/com/khtime/board/model/dao/BoardDao.java
+++ Semi_project/src/com/khtime/board/model/dao/BoardDao.java
@@ -1,8 +1,14 @@
 package com.khtime.board.model.dao;
 
+import static com.khtime.common.JDBCTemplate.close;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
@@ -23,4 +29,36 @@ public class BoardDao {
 	      }
 	      
 	   }
+	   
+	   
+	   public int selectListCount(Connection conn) {
+		      
+		      int listCount = 0;
+		      
+		      PreparedStatement pstmt = null;
+		      
+		      ResultSet rset = null;
+		      
+		      String sql = prop.getProperty("selectListCount");
+		      
+		      try {
+		         pstmt = conn.prepareStatement(sql);
+		         rset = pstmt.executeQuery();
+		         
+		         if(rset.next()) {
+		            listCount = rset.getInt("COUNT");
+		         }
+		         
+		      } catch (SQLException e) {
+		         e.printStackTrace();
+		      } finally {
+		         close(rset);
+		         close(pstmt);
+		      }
+		      return listCount;
+		   }
+	   
+	   
+	   
+	   
 }
