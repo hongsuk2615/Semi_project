f0fcb632a6e864c403796e4614d6341381e26839
llljjjsss123 <wnstjr8923@naver.com>
2023-04-09 13:44:52 +0900

    친구요청 수락 거절
    수락 거절

--- Semi_project/src/com/khtime/friend/controller/FriendDenyController.java
+++ Semi_project/src/com/khtime/friend/controller/FriendDenyController.java
@@ -1,8 +1,6 @@
 package com.khtime.friend.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,16 +12,16 @@ import com.khtime.friend.model.service.FriendService;
 import com.khtime.member.model.vo.Member;
 
 /**
- * Servlet implementation class FreindListController
+ * Servlet implementation class FriendDenyController
  */
-@WebServlet("/friend.me")
-public class FriendListController extends HttpServlet {
+@WebServlet("/frienddeny.me")
+public class FriendDenyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public FriendListController() {
+    public FriendDenyController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,26 +31,21 @@ public class FriendListController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int loginUserNo = ((Member)request.getSession().getAttribute("loginUser")).getUserNo();
-		ArrayList<Member> list = new FriendService().friendlist(loginUserNo);
-		request.setAttribute("list", list);//친구목록
-		ArrayList<Member> list2 = new FriendService().friendlist2(loginUserNo);
-		request.setAttribute("list2", list2);//친구 요청 목록
-		ArrayList<Member> list3 = new FriendService().friendlist3(loginUserNo);
-		request.setAttribute("list3", list3);//친구 요청 받은 목록
-		request.getRequestDispatcher("views/friend/friendList.jsp").forward(request, response);	
+		int senderUserNo = Integer.parseInt(request.getParameter("senderUserNo"));
+		int result = new FriendService().friendDeny(loginUserNo,senderUserNo);
+		System.out.println(result);
+		Gson gson = new Gson();
+		response.setContentType("application/json; charset=UTF-8");
+		gson.toJson(result,response.getWriter());
+		System.out.println(result);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int loginUserNo = ((Member)request.getSession().getAttribute("loginUser")).getUserNo();
-		int senderUserNo = Integer.parseInt(request.getParameter("senderUserNo"));
-		int result = new FriendService().friendaccept(loginUserNo,senderUserNo);
-		System.out.println(result);
-		Gson gson = new Gson();
-		response.setContentType("application/json; charset=UTF-8");
-		gson.toJson(result,response.getWriter());
-		System.out.println(result);
-}
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
+
+}
